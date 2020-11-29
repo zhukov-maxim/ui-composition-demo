ed7856fd1a5d270a60c07a8604255d706ff10939
Maxim Zhukov <zhkv.mxm@gmail.com>
2020-11-29 17:03:22 +0100

    Add CORS support

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const dates = require('./demo-responses/dates.json');
 const log20201125 = require('./demo-responses/2020-11-25.json');
 const log20201126 = require('./demo-responses/2020-11-26.json');
@@ -6,6 +8,8 @@ const log20201127 = require('./demo-responses/2020-11-27.json');
 
 const app = express();
 const port = 4201;
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Logs API'));
 app.get('/dates', (req, res) => res.send(dates));
